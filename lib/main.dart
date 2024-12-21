d3736f2ff25e6142dd0e95c3280688c4b26ba124
Ottavio Lucri De Souza <ottaviolucrinot@gmail.com>
2024-12-21 17:07:45 -0300

    Criando componente separado de lista e do formulario, e mudando a main para isso

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:money_control/transaction.dart';
+import 'package:money_control/components/trasactions_list.dart';
 
 main() {
   runApp(const MoneyControlApp());
@@ -18,6 +19,8 @@ class MoneyControlApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
+
+
   final _transactions = [
     Transaction(
       id: 't1',
@@ -43,10 +46,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Money Control - Financial estability!'),
+        title: Text('Money Control - Financial stability!'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -59,61 +61,10 @@ class HomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        tr.value.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.yellow,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tr.title,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                              ),
-                            Text(
-                              tr.date.toString(),
-                              style: TextStyle(
-                                color: const Color.fromARGB(255, 110, 110, 110),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+
+          TransactionList(_transactions),
+
+          
         ],
       ),
     );
---
+++ lib/transaction_form.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Card(
+          elevation: 5,
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: titleController,
+                cursorColor: Colors.blue[900],
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                ),
+              ),
+              TextField(
+                controller: valueController,
+                decoration: InputDecoration(
+                  labelText: 'Valor (R\$)',
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.blue[900],
+                    ),
+                    onPressed: () {},
+                    child: Text('Nova Transação'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
